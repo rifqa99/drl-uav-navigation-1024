2cae68606ea52d741c61e8a4628d677c448926b0
rifqa99 <rifqa.mubark@gamil.com>
2026-06-23 11:04:29 +0300

    train 512 resume

--- train-1024-resume.py
+++ train-1024-resume.py
@@ -45,8 +45,8 @@ def train_dqn_dynamic(reward_mode="standard", checkpoint_file=None):
     buffer_capacity = 50000
     target_update_frequency = 10
 
-    old_save_dir = f"/content/drive/MyDrive/drl-uav-512/outputs_dynamic_standard/checkpoints"
-    save_dir = os.path.join(old_save_dir, "checkpoints_new")
+    old_save_dir = f"/content/drive/MyDrive/drl-uav-512/outputs_dynamic_standard/checkpoints/checkpoints_new"
+    save_dir = os.path.join(old_save_dir, "checkpoints_new2")
     checkpoint_dir = os.path.join(save_dir, "checkpoints")
     os.makedirs(checkpoint_dir, exist_ok=True)
 
